fbe75a3c78cf51b050e95adf7c97b68fbb08cf54
Mike Kelly <mikekelly321@gmail.com>
2012-11-26 19:58:01 +0000

    fix spec

--- spec/hactor/http/response_spec.rb
+++ spec/hactor/http/response_spec.rb
@@ -5,17 +5,17 @@ describe Hactor::HTTP::Response do
   let(:codec) { mock }
   let(:body) { mock }
   let(:wrapped_response) { mock }
-  let(:response) {
-    Hactor::HTTP::Response.new(wrapped_response,
-                               codec: codec,
-                               body: body
-                              )}
 
   describe "#follow" do
     let(:http_client) { mock }
     let(:rel) { stub }
     let(:link) { stub }
     let(:actor) { stub }
+    let(:response) {
+      Hactor::HTTP::Response.new(wrapped_response,
+                                 codec: codec,
+                                 body: body
+                                )}
 
     it "gets the uri for the link with supplied rel" do
       options = { actor: actor, http_client: http_client }
@@ -29,11 +29,16 @@ describe Hactor::HTTP::Response do
   end
 
   describe "#body" do
-    let(:body) { nil }
+    let(:response) {
+      Hactor::HTTP::Response.new(wrapped_response,
+                                 codec: codec
+                                )}
+    let(:body) { mock }
 
     it "passes itself into the codec" do
       sentinel = stub
-      codec.should_receive(:new).with(response).and_return(sentinel)
+      wrapped_response.should_receive(:body).and_return(body)
+      codec.should_receive(:new).with(body).and_return(sentinel)
       response.body.should == sentinel
     end
   end
