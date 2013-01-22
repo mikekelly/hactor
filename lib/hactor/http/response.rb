e65f616a413ab7b4de57ed4d452f38c830d0fc29
Mike Kelly <mikekelly321@gmail.com>
2013-01-22 18:23:18 +0000

    name things properly pls

--- lib/hactor/http/response.rb
+++ lib/hactor/http/response.rb
@@ -4,10 +4,10 @@ require 'hactor/hal/document'
 module Hactor
   module HTTP
     class Response < SimpleDelegator
-      attr_reader :request_client, :codec
+      attr_reader :http_client, :codec
 
       def initialize(response, options={})
-        @request_client = options.fetch(:http_client)
+        @http_client = options.fetch(:http_client)
         @codec = options.fetch(:codec) { Hactor::HAL::Document }
         @body = options[:body]
         super(response)
@@ -15,7 +15,7 @@ module Hactor
 
       def follow(rel, options={})
         actor = options.fetch(:actor) { Hactor::NullActor.new }
-        client = options.fetch(:http_client) { request_client }
+        client = options.fetch(:http_client) { http_client }
 
         link = body.link(rel)
         client.follow(link, context_url: env[:url], actor: actor)
