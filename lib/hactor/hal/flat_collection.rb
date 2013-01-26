b9b060ef09c9590425df9b1665a645698d0a34ae
Mike Kelly <mikekelly321@gmail.com>
2013-01-26 15:06:27 +0000

    flat collection passes the parent resource as context to new items

--- lib/hactor/hal/flat_collection.rb
+++ lib/hactor/hal/flat_collection.rb
@@ -1,23 +1,27 @@
 module Hactor
   module HAL
     class FlatCollection < Array
-      attr_reader :item_class
+      attr_reader :parent, :item_class
 
-      def initialize(hash, options={})
+      def initialize(hash, options)
+        @parent = options.fetch(:parent)
         @item_class = options.fetch(:item_class)
         super flatten(hash)
       end
 
       private
       def flatten(hash)
+        hash ||= {}
         arr = []
-          hash.each do |rel, value|
-            if value.is_a? Array
-              arr += value.map { |obj| item_class.new(obj.merge(rel: rel)) }
-            else
-              arr.push item_class.new(value.merge(rel: rel))
-            end
+
+        hash.each do |rel, value|
+          if value.is_a? Array
+            arr += value.map { |obj| item_class.new(obj, rel: rel, context: parent) }
+          else
+            arr.push item_class.new(value, rel: rel, context: parent)
           end
+        end
+
         arr
       end
     end
