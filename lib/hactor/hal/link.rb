53f9b07ca3efd8b98abb1622867d1c7bc640b526
Mike Kelly <mikekelly321@gmail.com>
2013-01-26 15:07:54 +0000

    Link takes rel and parent resource as options

--- lib/hactor/hal/link.rb
+++ lib/hactor/hal/link.rb
@@ -2,6 +2,14 @@ require 'ostruct'
 
 module Hactor
   module HAL
-    class Link < OpenStruct;end
+    class Link < OpenStruct
+      attr_reader :rel, :parent_resource
+
+      def initialize(obj, options)
+        @rel = options.fetch(:rel)
+        @parent_resource = options.fetch(:context)
+        super(obj)
+      end
+    end
   end
 end
---
+++ lib/hactor/hal/null_link.rb
@@ -0,0 +1,8 @@
+module Hactor
+  module HAL
+    class NullLink
+      def initialize(obj={}, options={});end
+      def href;end
+    end
+  end
+end
