1201ac16f703197bfee2c92e3c42b6597e846881
Mike Kelly <mikekelly321@gmail.com>
2013-01-26 16:23:57 +0000

    amend null_actor_spec slightly

--- spec/hactor/null_actor_spec.rb
+++ spec/hactor/null_actor_spec.rb
@@ -4,8 +4,6 @@ describe Hactor::NullActor do
   let(:response) { mock }
   describe "#call" do
     it "should raise an exception when called" do
-      response.should_receive(:inspect)
-
       expect {
         subject.call(response)
       }.to raise_exception(RuntimeError)
